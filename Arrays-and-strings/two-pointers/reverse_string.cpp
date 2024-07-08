160f1cdeeaa40ae253fad9443f8a433d0ca7a9b0
Sneha S <104543929+snehasadap@users.noreply.github.com>
2024-07-08 03:46:31 -0700

    Add files via upload

---
+++ Arrays-and-strings/two-pointers/reverse_string.cpp
@@ -0,0 +1,22 @@
+//reverse a string in place. use two pointers, one starting on left and the other on the right. to "reverse" the string, you swap the characters
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size() - 1;
+        char temp;
+        while (i < j){
+           temp = s[i]; //assign temp to the left char to ensure the left value is stored somewhere
+           s[i] = s[j]; //since we have temp, we can safely assign the right value to the left. now, the left is equal to the right value
+           s[j] = temp; //we take the temp which has the original value of the left and assign it to the right value to "swap" the indices   
+            i++; //once that is complete, we increment and decrement the left and right pointers, respectively, to repeat the process for the rest of the chars
+            j--;
+        }
+        
+        
+    }
+};
+
+//time complexity: O(n)
+//space complexity: O(1)

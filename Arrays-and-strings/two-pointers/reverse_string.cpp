fd6d887e1d4d590d3aae26eca7aaa8021ce6a663
Sneha S <104543929+snehasadap@users.noreply.github.com>
2024-07-08 08:20:43 -0400

    Update reverse_string.cpp

--- Arrays-and-strings/two-pointers/reverse_string.cpp
+++ Arrays-and-strings/two-pointers/reverse_string.cpp
@@ -8,9 +8,9 @@ public:
         char temp;
         while (i < j){
            temp = s[i]; //assign temp to the left char to ensure the left value is stored somewhere
-           s[i] = s[j]; //since we have temp, we can safely assign the right value to the left. now, the left is equal to the right value
+           s[i] = s[j]; //since we have temp, we can safely assign the right value to the left. now, the left index is equal to the right
            s[j] = temp; //we take the temp which has the original value of the left and assign it to the right value to "swap" the indices   
-            i++; //once that is complete, we increment and decrement the left and right pointers, respectively, to repeat the process for the rest of the chars
+            i++; //once that is complete, we increment and decrement the left and right indices, respectively, to repeat the process for the rest of the chars
             j--;
         }
         

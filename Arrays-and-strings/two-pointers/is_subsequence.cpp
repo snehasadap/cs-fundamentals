18acee0f48a07e1529e14e7cca0e271d2bfbf235
Sneha S <104543929+snehasadap@users.noreply.github.com>
2024-07-08 03:09:05 -0700

    Add files via upload

---
+++ Arrays-and-strings/two-pointers/is_subsequence.cpp
@@ -0,0 +1,20 @@
+//given two strings s and t, returns True if s is a subsequence of t and False otherwise
+//Ex: s = 'ace', t = 'a, b, c, d, e' --> True (it doesn't matter if they aren't consecutive)
+
+bool isSubsequence(string s, string t){
+    int i = 0; //string s pointer
+    int j = 0; //string t pointer
+
+    while (i < s.size() and j < t.size()){
+        if (s[i] == t[j]) { 
+            i++; //increment s pointer to move to next char
+        }
+        else{
+            j++; //increment t pointer to search for characters equal to s[i]
+        }
+    }
+    return i == s.size(); //if the value of i after loop doesn't equal the size of s, that means not all elements were found, so the function will return false. otherwise, it will return true
+}
+
+//time complexity: O(1)
+

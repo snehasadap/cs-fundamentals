12d17dd88bd5c8de3e14175f47e71ad0c1f5bd2a
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-07-18 21:16:42 -0400

    Create min-size-subarray-sum.cpp

---
+++ sliding-window/min-size-subarray-sum.cpp
@@ -0,0 +1,59 @@
+/*Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+ whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0 */
+
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //return minimum subarray, NOT maximum, so call the min function
+        //the moment we find that very next value that is greater than or equal to 7, we have found our window
+        //keep incrementing left ptr and decrementing each element that consists of the sum until we get zero
+        //create an outside loop. if the total sum of the array is less than the target, then result is zero
+        int sum = 0;
+        int left = 0;
+        int ans = INT_MAX; //it is not zero because if it was, then the output will always be zero
+        
+        for (int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+
+        if (sum < target){
+            return 0;
+        }
+        sum = 0;
+
+        for(int right = 0; right < nums.size(); right++){
+            sum += nums[right];
+            while(sum >= target){
+                ans = min(ans, right - left + 1); //this goes inside of the while loop. this will track any valid windows within current window by decrementing values from the left side. doing so will help find the min. if it was max, there is no need to execute function in while loop since we want the largest value anyway so we don't have the evaluate the subarrays within our window
+                sum -= nums[left];
+                left++;
+            }
+
+        }
+
+        return (ans == INT_MAX) ? 0 : ans;
+
+        
+
+    }
+};
+
+//complexity: O(n)

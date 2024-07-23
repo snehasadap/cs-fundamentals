63618479fcb99459fe843b917e63e1f4fc21080a
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-07-23 19:14:24 -0400

    Update longest-repeating-character-replacement.cpp

--- sliding-window/longest-repeating-character-replacement.cpp
+++ sliding-window/longest-repeating-character-replacement.cpp
@@ -37,7 +37,7 @@ public:
         while (right < s.size()) {
             count[s[right] - 'A']++; // Increment the frequency of the current character in the window
 
-            maxCount = max(maxCount, count[s[start] - 'A']); //finds character that occurs the most frequently in the window. the one that appears less will be the one that's associated with k (so this will start B regardless of how many As preceed it)
+            maxCount = max(maxCount, count[s[start] - 'A']); //finds character that occurs the most frequently in the window. the one that appears less will be the one that's associated with k
 
         // Check if the current window size minus the maximum frequency character is greater than k
             if (right - left + 1 - maxCount > k) { //if the count of B in this case is greater than k, then the window is no longer valid. If maxCount refers to the count of A, then start - left + 1 (the size of the array) - maxCount (A count) refers to B

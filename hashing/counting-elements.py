2b8c5e7b6bb4f5bb4dcdf990f740a3b99a696b52
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-07-30 01:43:10 -0400

    Create counting-elements.py

---
+++ hashing/counting-elements.py
@@ -0,0 +1,31 @@
+'''
+Given an integer array arr, count how many elements x there are, such that x + 1 is also in arr. If there are duplicates in arr, count them separately.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3]
+Output: 2
+Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+Example 2:
+
+Input: arr = [1,1,3,3,5,5,7,7]
+Output: 0
+Explanation: No numbers are counted, cause there is no 2, 4, 6, or 8 in arr.
+ 
+
+
+'''
+
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        count = 0
+        
+        elements_dict = {num: None for num in arr}
+        
+        for val in arr:
+            if ((val + 1) in elements_dict):
+                count += 1
+        
+        return count

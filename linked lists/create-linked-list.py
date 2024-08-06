2d80eef85481c368d15972b4cbdcca30cba2335f
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-05 20:35:43 -0400

    Create create-linked-list.py

---
+++ linked lists/create-linked-list.py
@@ -0,0 +1,15 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+one = ListNode(1)
+two = ListNode(2)
+three = ListNode(3)
+one.next = two
+two.next = three
+head = one
+
+print(head.val)
+print(head.next.val)
+print(head.next.next.val)

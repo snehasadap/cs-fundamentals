5ad19bc3db34e268cbfa7cda92d87ff850f20f7e
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-05 21:03:36 -0400

    Create double-linked-list.py

---
+++ linked lists/double-linked-list.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+# Let node be the node at position i
+def add_node(node, node_to_add):
+    prev_node = node.prev
+    node_to_add.next = node
+    node_to_add.prev = prev_node
+    prev_node.next = node_to_add
+    node.prev = node_to_add
+
+# Let node be the node at position i
+def delete_node(node):
+    prev_node = node.prev
+    next_node = node.next
+    prev_node.next = next_node
+    next_node.prev = prev_node

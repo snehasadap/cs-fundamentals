84497dd3727505ce7bdcb97dd1f3e7e25f7c7c56
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-09 23:20:05 -0400

    Create valid_parentheses.py

---
+++ stacks and queues/valid_parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        matching = {"(": ")", "[": "]", "{": "}"}
+        
+        for c in s:
+            if c in matching: # if c is an opening bracket
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                
+                previous_opening = stack.pop()
+                if matching[previous_opening] != c:
+                    return False
+ 
+        return not stack

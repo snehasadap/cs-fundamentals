da2bb288c04f15066389edc4e35cb47f04544be6
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-12 00:51:53 -0400

    Create same-tree.py

---
+++ trees and graphs/same-tree.py
@@ -0,0 +1,30 @@
+'''
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+'''
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p == None and q == None:
+            return True
+        
+        if p == None or q == None:
+            return False
+        
+        if p.val != q.val:
+            return False
+        
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return left and right

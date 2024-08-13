28e4164937ac39813cc25aa84ecb0c5dac2510ee
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-12 21:52:31 -0400

    Create bfs.py

---
+++ trees and graphs/bfs.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def print_all_nodes(root):
+    queue = deque([root])
+    while queue:
+        nodes_in_current_level = len(queue)
+        # do some logic here for the current level
+
+        for _ in range(nodes_in_current_level):
+            node = queue.popleft()
+            
+            # do some logic here on the current node
+            print(node.val)
+
+            # put the next level onto the queue
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)

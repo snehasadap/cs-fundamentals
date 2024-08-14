0a6b60160c62f8e5de5461eecce963153e53be69
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-13 19:28:01 -0700

    Create minimum-absolute-difference-in-bst.py

---
+++ trees and graphs/minimum-absolute-difference-in-bst.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return
+            
+            left = dfs(node.left)
+            values.append(node.val)
+            right = dfs(node.right)
+
+        values = []
+        dfs(root)
+        ans = float("inf")
+        for i in range(1, len(values)):
+            ans = min(ans, values[i] - values[i - 1])
+        
+        return ans

66907496effcda43e9f224acf0f6c7a67ceb7f06
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-14 03:48:15 -0700

    Update valid_parentheses.py

--- stacks and queues/valid_parentheses.py
+++ stacks and queues/valid_parentheses.py
@@ -7,11 +7,11 @@ class Solution:
             if c in matching: # if c is an opening bracket
                 stack.append(c)
             else:
-                if not stack:
+                if not stack: #implies that we start with miscellanious character or closed parentheses
                     return False
                 
                 previous_opening = stack.pop()
-                if matching[previous_opening] != c:
+                if matching[previous_opening] != c: #checks if the top of the stack's key is not equal to c
                     return False
  
         return not stack

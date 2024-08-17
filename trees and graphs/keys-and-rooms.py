372105760269c3eb919f5970c7f661388c3f7ccc
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-16 19:27:57 -0700

    Create keys-and-rooms.py

---
+++ trees and graphs/keys-and-rooms.py
@@ -0,0 +1,13 @@
+#link: https://leetcode.com/problems/keys-and-rooms/
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        def dfs(node):
+            for neighbor in rooms[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+            
+        seen = {0}
+        dfs(0)
+        return len(seen) == len(rooms)

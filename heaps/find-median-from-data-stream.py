fe5acf36395861f952b202942436dc52ee5ac60b
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-25 16:37:04 -0700

    Create find-median-from-data-stream.py

---
+++ heaps/find-median-from-data-stream.py
@@ -0,0 +1,18 @@
+#link: https://leetcode.com/problems/find-median-from-data-stream/
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.max_heap, -num)
+        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        return (self.min_heap[0] - self.max_heap[0]) / 2

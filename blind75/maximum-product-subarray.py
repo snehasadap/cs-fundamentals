8cef86e793a83c098032ec4afab1a120ad4d30e0
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-10-30 23:28:33 -0400

    Create maximum-product-subarray.py

---
+++ blind75/maximum-product-subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        #dynamic programming
+        res = max(nums) 
+        curMin, curMax = 1, 1
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            tmp = curMax * n #so we don't use the curMax after it's calculated
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+
+        return res
+
+
+        
